c32f935d59ca75f93ccb89da7d64ff42d6c01c23
Guillermo-CM <Guillermo.castm@hotmail.com>
2023-10-10 14:36:40 -0700

    First commit

---
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+  @override
+  State<Counter> createState() => _CounterState();
+}
+class _CounterState extends State<Counter> {
+  int countador = 10;
+  String textito = "Clicks";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$countador',
+              style: const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+          Text(textito, 
+          style: const TextStyle(fontSize: 60))
+          ],
+        ),
+      ),
+      floatingActionButton:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+                setState(() {
+                countador ++;
+                });
+            },
+              child: const Icon(Icons.plus_one),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () {
+                setState(() {
+                countador --;
+                });
+              }, child: const Icon(Icons.exposure_minus_1)),
+        ],
+      ),
+    );
+  }
+}
